6cf805d4e61f0015e02a0f119e9f19982cd04a83
Marilzon <maxmaril@hotmail.com>
2023-02-11 09:12:41 -0300

    update: c++ setup, hello world classic and basic of variables

---
+++ c++/02-variables.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	/* Declaring a variable called `integer` and it is a `int` type. */
+	int integer; // 1, 2, 3
+
+	/* Declaring a variable called `character` and it is a `char` type. */
+	char character; // "A"
+
+	/* Declaring a variable called `vetorOfCharacteres` and it is a `char` type. */
+	char vetorOfCharacteres[20];
+
+	/* Declaring a variable called `biggerValue` and it is a `double` type. */
+	double biggerValue; // 2.4999999, bigger then float
+
+	/* Declaring a variable called `floatValue` and it is a `float` type. */
+	float floatValue; // 2.49
+
+	/* A boolean variable. */
+	bool trueOrFalse; // true or false
+
+	/* A string variable. */
+	string userName; // "the string like a `line` of characteres"
+}
