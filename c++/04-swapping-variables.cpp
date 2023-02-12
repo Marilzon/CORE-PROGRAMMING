edea4f015540ec2f0892b8165bc49d7c55a673c1
Marilzon <maxmaril@hotmail.com>
2023-02-12 10:11:18 -0300

    update: swapping polarity basics

---
+++ c++/04-swapping-variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int positiveValue = 10;
+	int negativeValue = -5;
+
+	cout << positiveValue << endl;
+	cout << negativeValue << endl;
+
+	/* Multiply * -1 to invert polarity. */
+	cout << positiveValue * -1 << endl;
+	cout << negativeValue * -1 << endl;
+}
