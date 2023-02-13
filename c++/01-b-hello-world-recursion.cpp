c4821825bc027e73af7d1120c785e826b0c54b0f
Marilzon <maxmaril@hotmail.com>
2023-02-12 23:55:43 -0300

    update: hello world using recursionm its complexx!

--- c++/01-b-hello-world-recursion.cpp
+++ c++/01-b-hello-world-recursion.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * The function prints the message "hello, Developer!" to the console
+ *
+ * @param message The message to print.
+ * @param index The index of the character in the string that we want to print.
+ *
+ * @return Nothing
+ */
+
 void print(const char *message, int index)
 {
 	if (message[index] == '\0')
