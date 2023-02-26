da91726e3561e7cbc6cbd3143919049d04768eb1
Marilzon <maxmaril@hotmail.com>
2023-02-26 00:56:47 -0300

    update: increasing type annotation examples

---
+++ typescript/src/01-string.js
@@ -0,0 +1,10 @@
+var message = "Hello Developer, Im Ghost!";
+var age = 30;
+var isDeveloper = true;
+var stacks = ['Javascript', 'Typescript', 'Golang'];
+var yearsOfEach = [4, 2, 1];
+var car;
+car = { name: 'Monza', model: 'sedan', year: 2002 };
+function multiply(x, y) {
+    return x * y;
+}
--- typescript/src/01-string.ts
+++ typescript/src/01-string.ts
@@ -1,1 +1,15 @@
 let message: string = "Hello Developer, Im Ghost!";
+let age: number = 30;
+let isDeveloper: boolean = true;
+let stacks: string[] = ['Javascript', 'Typescript', 'Golang']
+let yearsOfEach: number[] = [4, 2, 1];
+let car: {
+	name: string;
+	model: string;
+	year: number;
+};
+car = { name: 'Monza', model: 'sedan', year: 2002};
+
+function multiply(x: number, y: number): number {
+	return x * y;
+}
