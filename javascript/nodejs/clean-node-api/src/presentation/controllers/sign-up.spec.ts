5fd1b500e5bb181bd6d83bb2fd20f3ce48af9e3f
Marilzon <maxmaril@hotmail.com>
2023-04-06 20:38:53 -0300

    test: improving and destructing properties from SignUpController

--- javascript/nodejs/clean-node-api/src/presentation/controllers/sign-up.spec.ts
+++ javascript/nodejs/clean-node-api/src/presentation/controllers/sign-up.spec.ts
@@ -15,4 +15,19 @@ describe("SignUp controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error("Missing param: name"));
   });
+
+  test("should return 400 if no name is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_confirmation",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: email"));
+  });
 });
