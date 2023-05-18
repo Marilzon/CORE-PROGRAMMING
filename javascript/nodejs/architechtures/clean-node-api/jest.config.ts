b95dabc84c921273c069ed474c394cfe62d80d03
Marilzon <maxmaril@hotmail.com>
2023-05-17 21:59:42 -0300

    feature: many

--- javascript/nodejs/architechtures/clean-node-api/jest.config.ts
+++ javascript/nodejs/architechtures/clean-node-api/jest.config.ts
@@ -1,9 +1,7 @@
 export default {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: 'node',
   transform: {
